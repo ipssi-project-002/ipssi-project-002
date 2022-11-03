7fe81c6fea8169a9669a3ae63e3dc405ec40d90d
Thomas Léveillé <leveille.thomas77@gmail.com>
2022-11-03 14:30:39 +0100

    spaces

--- src/Controller/DishController.php
+++ src/Controller/DishController.php
@@ -6,12 +6,15 @@ class DishController extends DefaultController {
     public function view(array $params) {
         $this->render('Dish/view', $params);
     }
+
     public function edit(array $params) {
         $this->render('Dish/edit', $params);
     }
+
     public function add(array $params) {
         $this->render('Dish/add', $params);
     }
+    
     public function delete(array $params) {
         $this->render('Dish/delete', $params);
     }
