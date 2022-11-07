627b9ff6bc522809fc243ed637334039fb4e9570
Thomas Léveillé <leveille.thomas77@gmail.com>
2022-11-07 11:32:58 +0100

    minor change on home.php

--- src/Template/Home/home.php
+++ src/Template/Home/home.php
@@ -34,7 +34,6 @@
               <p class="card-text"><?= $dish->getName() ?></p>
               <div class="d-flex justify-content-between align-items-center">
                 <div class="btn-group">
-                  <button type="button" class="btn btn-sm btn-outline-secondary">Voir</button>
                   <button type="button" class="btn btn-sm btn-outline-secondary">Ajouter au panier</button>
                 </div>
                 <small class="text-muted"><?= \App\Utils::formatPrice($dish->getPrice()) ?></small>
